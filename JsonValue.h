3eb7bf4485d5c1ca16612e0f3a7c31c0c350139b
ArsenDavtyan-1 <arsend704@gmail.com>
2024-12-12 18:59:36 +0400

    Improving JsonValue class in JsonValue.h

--- JsonValue.h
+++ JsonValue.h
@@ -4,10 +4,37 @@
 #include <iostream>
 #include <variant>
 #include <string>
+#include <map>
+#include <vector>
 
 
-//using jsonValue = std::variant<std::nullptr_t, bool, int, double, std::string>;
+class JsonValue;
 
+
+using JsonObject = std::map < std::string, JsonValue>;
+using JsonArray = std::vector<JsonValue>;
+using JsonValueType = std::variant<std::nullptr_t, bool, double, std::string, JsonArray, JsonObject>;
+
+
+class JsonValue
+{
+	JsonValueType value;
+	
+	JsonValue() : value(nullptr) {}
+	JsonValue(std::nullptr_t) : value(nullptr) {}
+	JsonValue(bool b) : value(b) {}
+	JsonValue(double numb) : value(numb) {}
+	JsonValue(std::string& str) : value(str) {}
+	JsonValue(JsonObject& obj) : value(obj) {}
+	JsonValue(JsonArray& arr) : value(arr) {}
+
+	bool isNull() const { return std::holds_alternative<std::nullptr_t>(value); }
+	bool isBool() const { return std::holds_alternative<bool>(value); }
+	bool isNumber() const { return std::holds_alternative<double>(value); }
+	bool isString() const { return std::holds_alternative<std::string>(value); }
+	bool isArray() const { return std::holds_alternative<JsonArray>(value); }
+	bool isObject() const { return std::holds_alternative<JsonObject>(value); }
+};
 
 
 #endif ! JSON_VALUE_H
