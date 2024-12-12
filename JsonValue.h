f134d20bab16a50f9b5259e4ec38304d170e44a0
ArsenDavtyan-1 <arsend704@gmail.com>
2024-12-12 17:08:03 +0400

    adding JsonValue.* files

---
+++ JsonValue.h
@@ -0,0 +1,13 @@
+#ifndef JSON_VALUE_H
+#define JSON_VALUE_H
+
+#include <iostream>
+#include <variant>
+#include <string>
+
+
+//using jsonValue = std::variant<std::nullptr_t, bool, int, double, std::string>;
+
+
+
+#endif ! JSON_VALUE_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <variant>
+#include <fstream>
+
+
+int main() {
+    return 0;
+}
