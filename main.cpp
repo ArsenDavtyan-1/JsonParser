88636aa38f70bca6083d52a0b9dbf198644118dd
ArsenDavtyan-1 <arsend704@gmail.com>
2024-12-13 00:24:28 +0400

    some experiments in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <variant>
 #include <fstream>
-
+#include "readJson.h"
 
 int main() {
+    std::string filename = "example.json";
+    std::string file = readFile(filename);
+    std::cout << file << std::endl;
     return 0;
 }
+
