ad9dd04a65ed1cea6fb67ef1364a88ca335a0c08
ArsenDavtyan-1 <arsend704@gmail.com>
2024-12-13 21:43:02 +0400

    checking parse functions works

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 #include <variant>
 #include <fstream>
 #include "readJson.h"
+#include "JsonValue.h"
+#include "Tokenizer.h"
 
-int main() {
-    std::string filename = "example.json";
-    std::string file = readFile(filename);
-    std::cout << file << std::endl;
+
+
+int main() 
+{
+    std::string json = R"({"key": "value", "number": 123, "array": [true, false, null]})";
+
+    Tokenizer tokenizer(json);
+
+    while (tokenizer.hasNextToken()) {
+        auto token = tokenizer.nextToken();
+        std::cout << "Type: " << static_cast<int>(token.type) << ", Value: " << token.value << std::endl;
+    }
+
     return 0;
 }
+
 
